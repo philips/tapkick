92e282a9b551abc2bfae0c55bea0ed6f64777c59
Chris Gilmer <chris.gilmer@gmail.com>
2011-11-30 14:29:10 -0800

    Added ordering for beer and fixed up the name to make it readable in the admin

--- web/webapp/beer/models.py
+++ web/webapp/beer/models.py
@@ -26,8 +26,16 @@ class Beer(models.Model):
     ibu = models.PositiveIntegerField(default=0, help_text="International Bitterness Units (IBU)")
     abv = models.FloatField(default=0.0, help_text="Alcohol by Volume")
 
+    class Meta:
+        ordering = ('-active', '-start_date')
+
     def __unicode__(self):
-        return u'%s, %s' % (self.name, self.beer_type)
+        name = u'%s, %s' % (self.name, self.beer_type)
+        if self.active:
+            name += u' (#%s active)' % (self.tap_number)
+        else:
+            name += u' (#%s)' % (self.tap_number)
+        return name
 
     def save(self, *args, **kwargs):
         """
