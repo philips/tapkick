08ac63837729e29b2ec2760febd4f0e64f40ce3a
Rob Szumski <rob.szumski@rackspace.com>
2011-11-30 16:53:56 -0800

    /get_tap/#/
    level (exists, needs name)
    amountLeft
    age
    volume
    ibu
    abv
    
    /get_graph/#/
    
    values (exists, needs name)
    total
    
    /get_last/#/
    
    name
    time
    gravitar
    
    /get_highest/#/
    
    name
    time
    gravitar
    
    /get_fastest/#/
    
    name
    time
    gravitar

--- design/js/beer.js
+++ design/js/beer.js
@@ -49,76 +49,171 @@ function setLevel(tapNumber, level) {
 
 }
 
+function updatePerson(tapID, elementID, name, time, gravitar) {
+
+	//NAME
+	$("#" + tapID + " ." + elementID).find("h3").html(name);
+	
+	//TIME
+	$("#" + tapID + " ." + elementID).find("span.detail").html("(" + time + ")");
+	
+	//GRAVITAR
+	$("#" + tapID + " ." + elementID).find("img").attr("src", gravitar);
+
+}
+
+function requestTapData(tapID) {
+
+	if(tapID == "one") {
+		number = 1;
+	} else if (tapID == "two") {
+		number = 2;
+	}
+	
+	//REQUEST DATA
+	$.ajax({
+		type: 'GET',
+		url: "/get_tap/" + number + "/",
+		success: function(data){
+			
+			console.log(data);
+			setLevel(tapID, data.level*100);
+
+		}
+
+	});
+	
+	$("#" + tapID + " .meta h2").text("name!");
+	$("#" + tapID + " .meta h4").text(data.amountLeft + " beers left");
+	$("#" + tapID + " .meta .age .item-value").text(data.age);
+	$("#" + tapID + " .meta .volume .item-value").text(data.volume);
+	$("#" + tapID + " .meta .ibu .item-value").text(data.ibu);
+	$("#" + tapID + " .meta .abv .item-value").text(data.abv);
+
+}
+
+function requestTapGraph(tapID) {
+
+	if(tapID == "one") {
+		number = 1;
+	} else if (tapID == "two") {
+		number = 2;
+	}
+	
+	//REQUEST DATA
+	$.ajax({
+		type: 'GET',
+		url: "/get_graph/" + number + "/",
+		success: function(data){
+			
+			generateChart(tapID, data.values);
+			
+			$("#" + tapID + " .chart-desc").text("Consumption Trend (" + data.total + " people)")
+
+		}
+
+	});
+	
+}
+
+function requestLastToDrink(tapID, elementID) {
+
+	if(tapID == "one") {
+		number = 1;
+	} else if (tapID == "two") {
+		number = 2;
+	}
+	
+	//REQUEST DATA
+	$.ajax({
+		type: 'GET',
+		url: "/get_last/" + number + "/",
+		success: function(data){
+			
+			console.log(data);
+			updatePerson(tapID, elementID, data.name, data.time, data.gravitar);
+
+		}
+
+	});
+		
+}
+
+function requestHighestConsumption(tapID, elementID) {
+
+	if(tapID == "one") {
+		number = 1;
+	} else if (tapID == "two") {
+		number = 2;
+	}
+	
+	//REQUEST DATA
+	$.ajax({
+		type: 'GET',
+		url: "/get_highest/" + number + "/",
+		success: function(data){
+			
+			console.log(data);
+			updatePerson(tapID, elementID, data.name, data.amount, data.gravitar);
+
+		}
+
+	});
+		
+}
+
+function requestFastestBeer(tapID, elementID) {
+
+	if(tapID == "one") {
+		number = 1;
+	} else if (tapID == "two") {
+		number = 2;
+	}
+	
+	//REQUEST DATA
+	$.ajax({
+		type: 'GET',
+		url: "/get_fastest/" + number + "/",
+		success: function(data){
+			
+			console.log(data);
+			updatePerson(tapID, elementID, data.name, data.time, data.gravitar);
+
+		}
+
+	});
+	
+}
+
 function updateValues() {
 
 	console.log("checking values...");
+
+	//TAP LEVEL
+	requestTapData("one");
+	requestTapData("two");
 	
-	//FIRST TAP DATA
-	$.ajax({
-		type: 'GET',
-		url: "/get_tap/1/",
-		success: function(data){
-			
-			console.log(data);
-			setLevel("one", data*100);
-
-		}
-
-	});
-
-	$.ajax({
-		type: 'GET',
-		url: "/get_graph/1/",
-		success: function(data){
-			
-			console.log(data);
-			generateChart("one", data);
-
-		}
-
-	});
-
-	//SECOND TAP DATA
-	$.ajax({
-		type: 'GET',
-		url: "/get_tap/2/",
-		success: function(data){
-			
-			console.log(data);
-			setLevel("two", data*100);
-
-		}
-
-	});
-
-	$.ajax({
-		type: 'GET',
-		url: "/get_graph/2/",
-		success: function(data){
-			
-			console.log(data);
-			generateChart("two", data);
-
-		}
-
-	});
+	//GRAPHS
+	requestTapGraph("one");
+	requestTapGraph("two");
 	
+	//LAST TO DRINK
+	requestLastToDrink("two", "last");
+	requestLastToDrink("one", "last");
+	
+	//HIGHEST CONSUPTION
+	requestHighestConsumption("two", "consumption");
+	requestHighestConsumption("one", "consumption");
+	
+	//FASTEST BEER
+	requestFastestBeer("two", "fastest");
+	requestFastestBeer("one", "fastest");
 	
 }
 
 $(document).ready(function(){
 
-	//ANIMATE TANK LEVEL
-	//var interval = setInterval("animate()", 100);	
-
-	//var interval = setInterval("updateValues()", 5000);
+	var interval = setInterval("updateValues()", 2000);
 	updateValues();
-	/*//SET TANK LEVEL
-	setLevel("one", Math.floor(Math.random()*100));
-	setLevel("two", Math.floor(Math.random()*100));
-	
-	//GENERATE CHART
-	generateChart("one", [3,4,5,6,4,6,9,3,2]);
-	generateChart("two", [3,4,5,6,4,6,9,3,2]);*/
 	
 });
